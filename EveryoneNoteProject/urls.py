423fe7d3214d1c0a7c8938f4f2c9df4fb3f2de34
obarakkyo <obarakkyo@icloud.com>
2024-01-24 19:52:28 +0900

    media settings

--- EveryoneNoteProject/urls.py
+++ EveryoneNoteProject/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+"""media用の設定"""
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('lecture/', include('lecture.urls')),
@@ -9,3 +13,7 @@ urlpatterns = [
     path('upload/', include('upload.urls')),
     path('userlogapp/', include('userlogapp.urls')),
 ]
+
+#開発の時はこれを記入する
+if settings.DEBUG:
+    urlpatterns+= static( settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
