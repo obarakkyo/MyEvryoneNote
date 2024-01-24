1a25353645a6c543b0a82ee9e85f1e8533560898
obarakkyo <obarakkyo@icloud.com>
2024-01-24 19:36:13 +0900

    mnvoiceapp url test

--- EveryoneNoteProject/urls.py
+++ EveryoneNoteProject/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lecture/', include('lecture.urls'))
+    path('lecture/', include('lecture.urls')),
+    path('mnvoice/', include('mnvoice.urls')),
+    # path('note/', include('note.urls')),
+    # path('upload/', include('upload.urls')),
+    # path('userlogapp/', include('userlogapp.urls')),
 ]
--- mnvoice/views.py
+++ mnvoice/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def mnvoice_base(request):
+    return render(request, 'mnvoice/mnvoice.html', context={})
