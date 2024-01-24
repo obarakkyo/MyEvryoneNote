504496e803f75c5bd03b6fc6def80ea453d07e9b
obarakkyo <obarakkyo@icloud.com>
2024-01-24 19:50:10 +0900

    Add search url html

--- userlogapp/urls.py
+++ userlogapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.userlogapp_base, name="login")
+    path('', views.userlogapp_base, name="login"),
+    path('search/', views.SearchFunction, name="search")
 ]
--- userlogapp/views.py
+++ userlogapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def userlogapp_base(request):
     return render(request, 'userlogapp/login.html', context={})
+
+def SearchFunction(request):
+    return render(request, 'userlogapp/search.html', context={})
