84ab553461ee569d1476d93f91bb96ce49c83193
obarakkyo <obarakkyo@icloud.com>
2024-01-24 19:57:59 +0900

    create and add lecture model

--- lecture/models.py
+++ lecture/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class LectureModel(models.Model):
+    name = models.CharField(max_length=255)
+    title1 = models.CharField(max_length=255)
+    title2 = models.CharField(max_length=255)
+    title3 = models.CharField(max_length=255)
+    title4 = models.CharField(max_length=255)
+    title5 = models.CharField(max_length=255)
+    title6 = models.CharField(max_length=255)
+    title7 = models.CharField(max_length=255)
+    title8 = models.CharField(max_length=255)
+    title9 = models.CharField(max_length=255)
+    title10 = models.CharField(max_length=255)
+    title11= models.CharField(max_length=255)
+    title12 = models.CharField(max_length=255)
+    title13= models.CharField(max_length=255)
+    title14 = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
