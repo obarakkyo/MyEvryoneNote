781e28b7ff9bb5e49246072722185bed231995da
obarakkyo <obarakkyo@icloud.com>
2024-01-26 13:11:45 +0900

    upload system output

--- note/views.py
+++ note/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from lecture.models import LectureModel
+from upload.models import UploadModel
 
 # Create your views here.
 def note_base(request, lecture_name):
     print(type(lecture_name), lecture_name)
     models = LectureModel.objects.filter(name=lecture_name)
+    print(models, type(models))
+    note_models = UploadModel.objects.filter(lecture_model=models.first())
+    print(note_models)
     return render(request, 'note/note.html', 
                   context={'lecture_name':lecture_name,
-                           'models'      :models})
+                           'models'      :models,
+                           'note_models':note_models},)
