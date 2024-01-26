008ac1ee2bb8298982212623b86d2e23645f457a
obarakkyo <obarakkyo@icloud.com>
2024-01-26 12:54:02 +0900

    note.html のレイアウト調整

--- note/views.py
+++ note/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from lecture.models import LectureModel
 
 # Create your views here.
 def note_base(request, lecture_name):
     print(type(lecture_name), lecture_name)
-    return render(request, 'note/note.html', context={'lecture_name':lecture_name})
+    models = LectureModel.objects.filter(name=lecture_name)
+    return render(request, 'note/note.html', 
+                  context={'lecture_name':lecture_name,
+                           'models'      :models})
