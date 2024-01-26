fac5e14fbbad052cb6f3b3f934cc80b789469cd8
obarakkyo <obarakkyo@icloud.com>
2024-01-26 16:12:10 +0900

    upload layout 回目に変更

--- EveryoneNoteProject/urls.py
+++ EveryoneNoteProject/urls.py
@@ -6,12 +6,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('userlogapp.urls')),
     path('admin/', admin.site.urls),
     path('lecture/', include('lecture.urls')),
     path('mnvoice/', include('mnvoice.urls')),
     path('note/', include('note.urls')),
     path('upload/', include('upload.urls')),
-    path('userlogapp/', include('userlogapp.urls')),
 ]
 
 #開発の時はこれを記入する
--- note/views.py
+++ note/views.py
@@ -4,11 +4,11 @@ from upload.models import UploadModel
 
 # Create your views here.
 def note_base(request, lecture_name, lecture_number):
-    print(type(lecture_name), lecture_name)
+    # print(type(lecture_name), lecture_name)
     models = LectureModel.objects.filter(name=lecture_name)
-    print(models, type(models))
+    # print(models, type(models))
     note_models = UploadModel.objects.filter(lecture_model=models.first(), lecture_choice=lecture_number)
-    print(note_models)
+    # print(note_models)
     return render(request, 'note/note.html', 
                   context={'lecture_name':lecture_name,
                            'models'      :models,
