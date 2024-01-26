fbd387063c50939414ecc54880b52a60e133ebab
obarakkyo <obarakkyo@icloud.com>
2024-01-26 14:49:53 +0900

    choice lectute_number note system

--- note/urls.py
+++ note/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'note_app'
 urlpatterns = [
-    path('<str:lecture_name>/', views.note_base, name="note_base")
+    path('<str:lecture_name>/<str:lecture_number>/', views.note_base, name="note_base")
 ]
--- note/views.py
+++ note/views.py
@@ -3,11 +3,11 @@ from lecture.models import LectureModel
 from upload.models import UploadModel
 
 # Create your views here.
-def note_base(request, lecture_name):
+def note_base(request, lecture_name, lecture_number):
     print(type(lecture_name), lecture_name)
     models = LectureModel.objects.filter(name=lecture_name)
     print(models, type(models))
-    note_models = UploadModel.objects.filter(lecture_model=models.first())
+    note_models = UploadModel.objects.filter(lecture_model=models.first(), lecture_choice=lecture_number)
     print(note_models)
     return render(request, 'note/note.html', 
                   context={'lecture_name':lecture_name,
