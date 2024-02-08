f3a3088037e743f77ecff462cbf3010146eb1c53
obarakkyo <obarakkyo@icloud.com>
2024-02-08 21:13:46 +0900

    mnvoice のページ作成

--- mnvoice/urls.py
+++ mnvoice/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 from . import views
+
+app_name = "mnvoice_app"
+
 urlpatterns = [
-    path('', views.mnvoice_base, name="mnvoice_base")
+    path('<str:lecture_name>/', views.mnvoice_base, name="mnvoice_base")
 ]
--- mnvoice/views.py
+++ mnvoice/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def mnvoice_base(request):
-    return render(request, 'mnvoice/mnvoice.html', context={})
+def mnvoice_base(request, lecture_name):
+    print('lecture_name=', lecture_name, type(lecture_name))
+
+    if request.method == 'POST':
+        print('POSTリクエストを受け取りました！')
+        print(request.POST['chat_document'])
+    
+    return render(request, 'mnvoice/mnvoice.html', context={'lecture_name':lecture_name})
