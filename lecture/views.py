a63162a937c1bb90cae3a0989105066e16275cd8
obarakkyo <obarakkyo@icloud.com>
2024-02-08 18:36:46 +0900

    css page遷移

--- lecture/views.py
+++ lecture/views.py
@@ -5,7 +5,12 @@ from django.contrib import messages
 def lecture_base(request):
     if request.method == "POST":
         search_word = request.POST['search_word']
-        lecture_names = LectureModel.objects.filter(name=search_word)
+        
+        if search_word == "":
+            messages.error(request, 'その講義は登録されていません。')
+            return render(request, 'userlogapp/search.html', context={})
+        
+        lecture_names = LectureModel.objects.filter(name__contains=search_word)
         if lecture_names:
             return render(request, 'lecture/lecture.html', context={'lecture_names':lecture_names})
         else:
