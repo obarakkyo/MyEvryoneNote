759b7b7696275c6ba3f979bb9ed4274640afb26a
obarakkyo <obarakkyo@icloud.com>
2024-02-13 16:54:16 +0900

    タブの画面遷移機能　完成

--- lecture/views.py
+++ lecture/views.py
@@ -16,5 +16,7 @@ def lecture_base(request):
         else:
             messages.error(request, 'その講義は登録されていません。')
             return render(request, 'userlogapp/search.html', context={})
-        
-    return render(request, 'lecture/lecture.html', context={})
+    
+    search_word = request.GET['search_word']
+    lecture_names = LectureModel.objects.filter(name=search_word)
+    return render(request, 'lecture/lecture.html', context={'lecture_names':lecture_names})
